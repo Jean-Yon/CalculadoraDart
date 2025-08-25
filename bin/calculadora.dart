c7bdc5c378e5c8aa0223920a57bbd0c7f60ae630
Pereira <jean66165@gmail.com>
2025-08-24 23:24:47 -0300

    Primeiro commit

---
+++ bin/calculadora.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+import 'package:math_expressions/math_expressions.dart';
+
+void main() {
+  print("=== CALCULADORA ===");
+  print("Digite a expressão:");
+  print("'Sair' para encerrar.\n");
+
+// Substitui Parser()
+  final parser = ShuntingYardParser();
+
+  while (true) {
+    stdout.write("Expressão: ");
+    String? entrada = stdin.readLineSync();
+
+    if (entrada == null || entrada.trim().toLowerCase() == 'sair') {
+      print("Encerrando...");
+      break;
+    }
+
+// usa o ShuntingYardParser
+    try {
+      Expression exp = parser.parse(entrada);
+      ContextModel cm = ContextModel();
+
+      double resultado = exp.evaluate(EvaluationType.REAL, cm);
+
+      // Tratamento de erro para verificar se o resultado é infinito ou não é um número
+      if (resultado.isInfinite || resultado.isNaN) {
+        print("Erro: divisão por zero ou operação inválida!\n");
+      } else {
+        print("Resultado: $resultado\n");
+      }
+    } catch (e) {
+      print("Expressão inválida! Tente novamente.\n");
+    }
+  }
+}
